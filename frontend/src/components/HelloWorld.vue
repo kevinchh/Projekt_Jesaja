f3c0929456ad0bfa18f29f49204f4996658ecb56
kevinchh <kevin.chhoeu22@gmail.com>
2022-01-18 23:36:12 +0100

    add progress bar and other visual tools

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,24 +4,40 @@
       <v-col cols="12">
         <h1>Feedback Evaluation</h1>
       </v-col>
-      <v-col cols="6">
-        <v-textarea
-          v-model = "inp"
-          name="name"
-          label="Test Eingabe"
-          id="id"
-          outlined
-          clearable
-          auto-grow
-          clear-icon="mdi-close-circle"
-        ></v-textarea>
+    </v-row>
+    <v-row class="grey d-flex lighten-5" justify-content="center">
+      <v-col cols="6"  align-self="center">
+        <v-card outlined width="600" height = "400">
+          <v-textarea
+            v-model = "inp"
+            name="name"
+            label="Test Eingabe"
+            id="id"
+            no-resize
+            full-width
+            height= 370
+            loading = false
+          ></v-textarea>
+          </v-card>
       </v-col >
-      <v-col cols = "6">
-        <v-container v-if="clicked">
-          <span v-for="i in entities" :key="i.text" v-bind:style = "{ 'color': i.color, 'text-align': 'left' }">
-            {{i.text}}
-          </span>
-        </v-container>
+      <v-col cols = "6" >
+          <v-card outlined width="600" height = "400">
+            <div v-if="loading">
+                <span :style = "{'text-align': 'center'}" >
+                  Progressing your input
+                </span>
+                <v-progress-linear
+                color="success"
+                indeterminate
+                rounded
+                height="10"
+                width = "4"
+              ></v-progress-linear>
+            </div>
+            <span v-for="i in entities" :key="i.text" :style = "{ 'color': i.color, 'text-align': 'left' }">
+              {{i.text}}
+            </span>
+          </v-card>
       </v-col>
     </v-row>
     <v-row>
@@ -37,7 +53,7 @@
           </v-icon>
         </v-btn>
       </v-col >
-    </v-row>
+    </v-row >
     <v-row>
   <v-expansion-panels>
     <v-expansion-panel>
@@ -87,7 +103,7 @@
       entities: [],
       inp: "Furthermore, asking for multiple opinions can benifit during competitions for a position slot, as cadidates needs to make decisions on what they need to say or do. For example, it can be helpful in situations like elections, both for the U.S. or simply in school. If a student is running for a position in office to represent his/her school, he/she can ask a widespread and diverse audience. First, asking other students is their best bet to obtaining information. Other students can inform him/her about what they want, like better water fountains, recess, or healthier food. Then, the student running can make changes to the way they run for the election, and on his/her speech, take a different approach. In addition, if the student running asks an adult, they will get to know a more realistic way the school can be improved. Since a student, even as a student officer, isn't able to make a significant change to a school, they can inform the school board about ways to make the school better.",
       out: "O Lead Position",
-      clicked: false
+      loading: false
     }),
     methods: {
       set_entities(data){
@@ -98,8 +114,11 @@
           temp.color = this.color_code[data["out_dict"][i][1]]
           this.entities.push(temp)
         }
+        this.loading = false
       },
       predict(){
+        this.entities = []
+        this.loading = true
         fetch("http://localhost:7000/predict", {
           method: "POST",
           headers: {
@@ -110,7 +129,6 @@
         }).then((res) => res.json())
         //.then(data => console.log(data))
         .then(data => this.set_entities(data))
-        .then(this.clicked = true)
         .catch(err => console.error("Failed to load: ",err))
       }
     },
@@ -118,4 +136,5 @@
     }
 
   }
+
 </script>
