0848e42fb3ad66910bd4c6853b45ed724170ab6d
Mark <mark.christy.george@rwth-aachen.de>
2022-01-20 20:48:17 +0100

    clean up app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,7 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Project Jesaja Logo"
           class="shrink mr-2"
           contain
           src="../data/logo_transparent.png"
@@ -25,15 +25,12 @@
     </v-app-bar>
 
     <v-main>
-      <v-btn @click="callPredict"></v-btn>
       <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
-import * as types from './store/types';
 import HelloWorld from './components/HelloWorld';
 
 export default {
@@ -42,17 +39,5 @@ export default {
   components: {
     HelloWorld,
   },
-
-  data: () => ({
-    test: 'Test text to test vuex store and api calls.'
-  }),
-  methods: {
-    ...mapActions({
-    predict: types.predictEval
-    }),
-    callPredict() {
-      this.predict(this.test);
-    }
-  }
 };
 </script>
