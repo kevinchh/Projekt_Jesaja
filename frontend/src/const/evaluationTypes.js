4a3f62702088f396180bfc2b192bdbd5272a0f69
Mark <mark.christy.george@rwth-aachen.de>
2022-01-22 22:11:23 +0100

    extend const function by ids, colors, and getters

--- frontend/src/const/evaluationTypes.js
+++ frontend/src/const/evaluationTypes.js
@@ -1,12 +1,44 @@
 export const colorCode = {
-    'O': "#595446", 
-    'Lead': "#967a2c", 
-    'Position': "#95962c", 
-    'Claim': "#76962c", 
-    'Counterclaim': "#2c3d20", 
-    'Rebuttal': "#203d2c",
-    'Evidence': "#21b59f", 
-    'Concluding Statement': "#13303b"
+    'O': {
+        light: "",
+        hover: "",
+        dark: ""
+    }, 
+    'Lead': {
+        light: "#6ce26d66",
+        hover: "#6CE26D",
+        dark: "#187719"
+    }, 
+    'Position': {
+        light: "#e19a6366",
+        hover: "#E19A63",
+        dark: "#894A1A"
+    }, 
+    'Claim': {
+        light: "#eba69a66",
+        hover: "#EBA69A",
+        dark: "#882B1B"
+    }, 
+    'Counterclaim': {
+        light: "#c2c34b66",
+        hover: "#C2C34B",
+        dark: "#7A7A29"
+    }, 
+    'Rebuttal': {
+        light: "#aa6cce66",
+        hover: "#AA6CCE",
+        dark: "#5C297A"
+    },
+    'Evidence': {
+        light: "#49b4dc66",
+        hover: "#49B4DC",
+        dark: "#175E78"
+    }, 
+    'Concluding Statement': {
+        light: "#54c67666",
+        hover: "#54C676",
+        dark: "#297A41"
+    }
 }
 
 export const queries = [
@@ -19,3 +51,26 @@ export const queries = [
     'Evidence', 
     'Concluding Statement'
 ];
+
+export const queryIds = [
+    'O',
+    'L',
+    'Pos',
+    'C',
+    'CC',
+    'R',
+    'Ev',
+    'ConSt'
+];
+
+const mapTwoArrays = (arr1, arr2) => {
+    return arr1.map((val, idx) => {
+        return {
+            [val]: arr2[idx]
+        };
+    });
+};
+
+export const getIdByQueryName = mapTwoArrays(queries, queryIds);
+
+export const getQueryNameById = mapTwoArrays(queryIds, queries);
